7f45e6fe847ebd889166e78bde190a0d71151ea9
Ebrahem Samer <ebrahemsamer2@gmail.com>
2018-10-10 16:05:40 +0700

    Set initialization function

--- script.js
+++ script.js
@@ -9,6 +9,25 @@ var budgetController = (function() {
 
 var UIController = (function() {
 
+	var DOMinputs = {
+		type: '.add__type',
+		description : '.add__description',
+		value: '.add__value',
+		addBtn: '.add__btn'
+	}
+
+	return {
+		getInputs: function() {
+			return {
+				type : document.querySelector(DOMinputs.type).value,
+				description : document.querySelector(DOMinputs.description).value,
+				value : document.querySelector(DOMinputs.value).value
+			}
+		},
+		getDOMstrings: function() {
+			return DOMinputs;
+		}
+	}
 })();
 
 
@@ -16,16 +35,29 @@ var UIController = (function() {
 // to connect budgetController with UIController
 var controller = (function(budgetCtrl , UICtrl){
 
-	function addItemClick() {
+	var DOM = UICtrl.getDOMstrings();
+	
+	var setupEventListeners = function() {
+		document.querySelector(DOM.addBtn).addEventListener('click' , addItemClick);
+		document.addEventListener('keypress' , function(event) {
+			if(event.keyCode === 13) {
+				addItemClick();
+			}
+		});
+	};
 
-	}
+	var addItemClick = function() {
+		var inputs = UICtrl.getInputs();
+	};
 
-	document.querySelector('.add__btn').addEventListener('click' , addItemClick());
-	document.addEventListener('keypress' , function(event) {
-		if(event.keyCode === 13) {
-			addItemClick();
+	return {
+		init: function() {
+			console.log('Application is started');
+			setupEventListeners();
 		}
-	});
+	};
 
 
 })(budgetController, UIController);
+
+controller.init();
